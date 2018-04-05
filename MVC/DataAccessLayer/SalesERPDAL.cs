157069c0575007c83f104bf0da1415cdc8b16926
M16-030B\E460 <rizky.eko.putra@gmail.com>
2018-04-05 14:43:59 +0700

    Lab 8
    Add Data Access layer to the project

---
+++ MVC/DataAccessLayer/SalesERPDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC.Models;
+using System.Data.Entity;
+
+namespace MVC.DataAccessLayer
+{
+    public class SalesERPDAL : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("TblEmployee");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- MVC/Models/EmployeeBusinessLayer.cs
+++ MVC/Models/EmployeeBusinessLayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MVC.DataAccessLayer;
 
 namespace MVC.Models
 {
@@ -9,26 +10,8 @@ namespace MVC.Models
     {
         public List<Employee> GetEmployees()
         {
-            List<Employee> employees = new List<Employee>();
-            Employee emp = new Employee();
-            emp.FirstName = "johnson";
-            emp.LastName = " fernandes";
-            emp.Salary = 14000;
-            employees.Add(emp);
-
-            emp = new Employee();
-            emp.FirstName = "michael";
-            emp.LastName = "jackson";
-            emp.Salary = 16000;
-            employees.Add(emp);
-
-            emp = new Employee();
-            emp.FirstName = "robert";
-            emp.LastName = " pattinson";
-            emp.Salary = 20000;
-            employees.Add(emp);
-
-            return employees;
+            SalesERPDAL salesDal = new SalesERPDAL();
+            return salesDal.Employees.ToList();
         }
     }
 }
