4f0e42c7493f12feac058c756a24973c7be5988f
M16-030B\E460 <rizky.eko.putra@gmail.com>
2018-04-06 18:28:44 +0700

    Lab 14
    Custom Server side validation

--- MVC/Models/Employee.cs
+++ MVC/Models/Employee.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using MVC.Validations;
 
 namespace MVC.Models
 {
@@ -10,7 +11,7 @@ namespace MVC.Models
     {
         [Key]
         public int EmployeeId { get; set; }
-        [Required(ErrorMessage = "Enter First Name")]
+        [FirstNameValidation]
         public string FirstName { get; set; }
         [StringLength(5, ErrorMessage = "Last Name length should not be greater than 5")]
         public string LastName { get; set; }
