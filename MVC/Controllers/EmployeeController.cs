06ec30fc66d3bc47a29885c033b9e1dc42a947bd
M16-030B\E460 <rizky.eko.putra@gmail.com>
2018-04-09 16:38:34 +0700

    Lab 22
    Add Footer

--- MVC/Controllers/EmployeeController.cs
+++ MVC/Controllers/EmployeeController.cs
@@ -55,6 +55,9 @@ namespace MVC.Controllers
                 empViewModels.Add(empViewModel);
             }
             employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.FooterData = new FooterViewModel();
+            employeeListViewModel.FooterData.CompanyName = "StepByStepSchools";
+            employeeListViewModel.FooterData.Year = DateTime.Now.Year.ToString();
             return View("Index", employeeListViewModel);
         }
 
