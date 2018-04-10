cab6743e487e39b1ad8b3763d472b58a9d64d716
M16-030B\E460 <rizky.eko.putra@gmail.com>
2018-04-10 16:11:28 +0700

    Lab 30
    Exception Handling – Log Exception

--- MVC/App_Start/FilterConfig.cs
+++ MVC/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using MVC.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MVC
@@ -7,7 +8,8 @@ namespace MVC
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
+            filters.Add(new EmployeeExceptionFilter());
         }
     }
 }
---
+++ MVC/Filters/EmployeeExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using MVC.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Filters
+{
+    public class EmployeeExceptionFilter : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            FileLogger logger = new FileLogger();
+            logger.LogException(filterContext.Exception);
+            base.OnException(filterContext);
+        }
+    }
+}
